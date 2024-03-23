6774d35dfc8b6442ecf52e4b222644bdf792c35b
giangnt <giangnt@tecapro.com.vn>
2024-03-24 00:03:53 +0700

    update

--- src/main/java/vn/com/gsoft/categories/service/impl/UserServiceImpl.java
+++ src/main/java/vn/com/gsoft/categories/service/impl/UserServiceImpl.java
@@ -2,28 +2,15 @@ package vn.com.gsoft.categories.service.impl;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cache.annotation.Cacheable;
-import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-import vn.com.gsoft.categories.constant.CachingConstant;
-import vn.com.gsoft.categories.constant.UserStatus;
-import vn.com.gsoft.categories.entity.Department;
-import vn.com.gsoft.categories.entity.User;
 import vn.com.gsoft.categories.model.system.Profile;
-import vn.com.gsoft.categories.repository.DepartmentRepository;
-import vn.com.gsoft.categories.repository.RoleRepository;
-import vn.com.gsoft.categories.repository.UserRepository;
 import vn.com.gsoft.categories.repository.feign.UserProfileFeign;
 import vn.com.gsoft.categories.service.UserService;
 
-import java.util.HashSet;
-import java.util.List;
 import java.util.Optional;
-import java.util.Set;
 
 @Service
 @Slf4j
